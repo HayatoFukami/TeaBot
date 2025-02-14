8c25abc506bade7de55d48dce59c10e9a8fce57f
hayato <hayato.fukami.0808@gmail.com>
2025-02-14 21:03:10 +0900

    Add rotating file handler for Discord logger
    Implemented a rotating file handler to manage log file size and backups. Updated the logging setup for better organization and improved log management, removing the old log handler in the bot's execution flow.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import logging.handlers
 import discord
 from discord.ext import commands as cmds
 from dotenv import load_dotenv
@@ -23,9 +24,24 @@ class TeaBot(cmds.Bot):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
 
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger("discord.http").setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,
+    backupCount=5
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 if __name__ == '__main__':
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     load_dotenv()
     token = os.getenv("TOKEN")
     bot = TeaBot()
-    bot.run(token, log_handler=handler)
+    bot.run(token, log_handler=None)
